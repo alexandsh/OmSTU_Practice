2f377f1a57e0dbd8316bceb9d832ea41de780cdd
Alexander Andreevich <sanyageyjopa@gmail.com>
2026-06-24 16:58:32 +0600

    feat: task01

---
+++ task01/Class1.cs
@@ -0,0 +1,25 @@
+﻿namespace task01;
+
+using System;
+using System.Linq;
+
+public static class StringExtensions
+{
+    public static string Reverse(this string str)
+    {
+        char[] charArray = str.ToCharArray();
+        Array.Reverse(charArray);
+        
+        return new string(charArray);
+    }
+
+    public static string Clean(this string str)
+    {
+        return new string(str.Where(c => !char.IsWhiteSpace(c) && !char.IsPunctuation(c)).ToArray());
+    }
+
+    public static bool IsPalindrome(this string str)
+    {
+        return str == "" ? false : string.Equals(str.Clean(), str.Clean().Reverse(), StringComparison.OrdinalIgnoreCase);
+    }
+}
