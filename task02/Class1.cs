9cbbc661da58abfc2a2d06a09f9bff40d6db3eda
Alexander Andreevich <sanyageyjopa@gmail.com>
2026-06-24 17:08:40 +0600

    feat: task02

---
+++ task02/Class1.cs
@@ -0,0 +1,35 @@
+﻿namespace task02;
+
+using System;
+using System.Linq;
+
+public class Student
+{
+    public string Name { get; set; } = string.Empty;
+    public string Faculty { get; set; } = string.Empty;
+    public List<int> Grades { get; set; } = new(); 
+}
+public class StudentService
+{
+    private readonly List<Student> _students;
+
+    public StudentService(List<Student> students) => _students = students;
+
+    public IEnumerable<Student> GetStudentsByFaculty(string faculty)
+        =>  _students.Where(s => s.Faculty == faculty);
+
+    public IEnumerable<Student> GetStudentsWithMinAverageGrade(double minAverageGrade) 
+        => _students.Where(s => s.Grades.DefaultIfEmpty().Average() >= minAverageGrade);
+
+    public IEnumerable<Student> GetStudentsOrderedByName()
+        => _students.OrderBy(s => s.Name);
+
+    public ILookup<string, Student> GroupStudentsByFaculty()
+        => _students.ToLookup(s => s.Faculty);
+
+    public string GetFacultyWithHighestAverageGrade()
+        => _students.GroupBy(s => s.Faculty)
+                    .OrderByDescending(g => g.SelectMany(s => s.Grades).DefaultIfEmpty().Average())
+                    .Select(g => g.Key)
+                    .FirstOrDefault() ?? string.Empty;
+}
