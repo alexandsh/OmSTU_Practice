50e5db21b02b510863ffcf4bf778f85633d0a27d
Alexander Andreevich <sanyageyjopa@gmail.com>
2026-07-04 16:25:23 +0600

    feat: task08

---
+++ CommandRunner/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+
+var assembly = Assembly.LoadFrom("../../../../FileSystemCommands/bin/Debug/net9.0/FileSystemCommands.dll");
+
+var directorySizeCommandType = assembly.GetType("FileSystemCommands.DirectorySizeCommand");
+var directorySizeCommandLaunch = directorySizeCommandType?.GetMethod("Execute", BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+var directorySizeCommandCopy = Activator.CreateInstance(directorySizeCommandType!, new object[] { "../../../../FileSystemCommands"});
+
+directorySizeCommandLaunch?.Invoke(directorySizeCommandCopy, null);
+
+var FindFilesCommandType = assembly.GetType("FileSystemCommands.FindFilesCommand");
+var FindFilesCommandLaunch = FindFilesCommandType?.GetMethod("Execute", BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+var FindFilesCommandCopy = Activator.CreateInstance(FindFilesCommandType!, new object[] { "../../../../FileSystemCommands" , "*.cs" });
+
+FindFilesCommandLaunch?.Invoke(FindFilesCommandCopy, null);
---
+++ FileSystemCommands/Class1.cs
@@ -0,0 +1,36 @@
+﻿namespace FileSystemCommands;
+
+using CommandLib;
+public class DirectorySizeCommand : ICommand
+{
+    private string _pathDirectory;
+    public long sizeDirectory { get; set; }
+    public DirectorySizeCommand(string pathDirectory)
+    {
+        _pathDirectory = pathDirectory;
+    }
+    public void Execute()
+    {
+        sizeDirectory = GetDirectorySize(new DirectoryInfo(_pathDirectory));
+    }
+    private static long GetDirectorySize(DirectoryInfo dir)
+    {
+        return dir.EnumerateFiles().Sum((f) => f.Length) + dir.EnumerateDirectories().Sum((d) => GetDirectorySize(d));
+    }
+}
+public class FindFilesCommand : ICommand
+{
+    public string mask { get; set; }
+    public string pathDirectory { get; set; }
+    public string[] foundFiles { get; set; } = [];
+
+    public FindFilesCommand(string pathDirectory, string mask)
+    {
+        this.pathDirectory = pathDirectory;
+        this.mask = mask;
+    }
+    public void Execute()
+    {
+        foundFiles = Directory.GetFiles(pathDirectory, mask);
+    }
+}
