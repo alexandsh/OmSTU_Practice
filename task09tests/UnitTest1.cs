393c0fb3f76918cc8ebda5d5601b192f95d1b6f8
Alexander Andreevich <sanyageyjopa@gmail.com>
2026-07-04 16:45:04 +0600

    feat: task09

---
+++ task09tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿namespace task09tests;
+
+using System.Reflection;
+using Xunit;
+using task09;
+
+public class Task09Tests
+{
+    [Fact]
+    public void PrintsTypeInfo_FromTask07()
+    {
+        var output = new StringWriter();
+        Console.SetOut(output);
+
+        var assemblyPath = typeof(task07.SampleClass).Assembly.Location;
+
+        task09.Program.Main(new string[] { assemblyPath });
+
+        Assert.Contains("class SampleClass", output.ToString());
+        Assert.Contains("attrs task07.DisplayNameAttribute", output.ToString());
+        Assert.Contains("attrs task07.VersionAttribute", output.ToString());
+        Assert.Contains("methods TestMethod", output.ToString());
+        Assert.Contains("methods get_Number", output.ToString());
+    }
+}
