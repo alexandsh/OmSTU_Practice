54e9a53f1227b157dd74bc0bc4fe52aa266619b5
Alexander Andreevich <sanyageyjopa@gmail.com>
2026-07-11 17:56:16 +0600

    fix: add compile-error check

--- task11/GenerateInRunTime.cs
+++ task11/GenerateInRunTime.cs
@@ -45,6 +45,14 @@ public class GenerateInRunTime
 
         using var memory = new MemoryStream();
         var result = compilationDll.Emit(memory);
+        if (!result.Success)
+        {
+            var errors = result.Diagnostics
+                .Where(d => d.Severity == DiagnosticSeverity.Error)
+                .Select(d => d.ToString());
+            throw new InvalidOperationException(
+                "compilation failed:\n" + string.Join("\n", errors));
+        }
         memory.Seek(0, SeekOrigin.Begin);
         var assembly = Assembly.Load(memory.ToArray());
         var calculator = (ICalculator)Activator.CreateInstance(assembly.GetType("task11.Calculator")!)!;
