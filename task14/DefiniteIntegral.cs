47f2d4bff74d442c6cc273a72720d8252defc833
Alexander Andreevich <sanyageyjopa@gmail.com>
2026-07-12 17:09:03 +0600

    feat: task14

---
+++ task14/DefiniteIntegral.cs
@@ -0,0 +1,43 @@
+﻿namespace task14;
+
+public class DefiniteIntegral
+{
+    public static double Solve(double a, double b, Func<double, double> function, double step, int threadsnumber)
+    {
+        var threads = new Thread[threadsnumber];
+        var borderThread = (b - a) / threadsnumber;
+        var res = 0.0;
+        var barrier = new Barrier(threadsnumber);
+        var locker = new object();
+
+        for (int i = 0; i < threadsnumber; i++)
+        {
+            var tindex = i;
+            threads[i] = new Thread(() =>
+            {
+                var bLeft = a + tindex * borderThread;
+                var bRight = tindex == threadsnumber ? b : bLeft + borderThread;
+                var tSum = 0.0;
+
+                for (var j = bLeft; j < bRight; j += step)
+                {
+                    tSum += 0.5 * (function(j) + function(Math.Min(j + step, bRight))) * ((Math.Min(j + step, bRight)) - j);
+                }
+
+                barrier.SignalAndWait();
+
+                lock (locker)
+                {
+                    res += tSum;
+                }
+            });
+            threads[i].Start();
+        }
+
+        foreach (var t in threads)
+        {
+            t.Join();
+        }
+        return res;
+    }
+}
