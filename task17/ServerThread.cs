e6d3363895abf9e53789285f87fbf1c3ee57c50f
Alexander Andreevich <sanyageyjopa@gmail.com>
2026-07-14 01:54:44 +0600

    feat: task17

---
+++ task17/ServerThread.cs
@@ -0,0 +1,82 @@
+﻿namespace task17;
+
+using System.Collections.Concurrent;
+using ICommand;
+public class ServerThread
+{
+    private Thread? thread;
+    private BlockingCollection<ICommand> queue = new();
+    private bool needHardStop = false;
+    private bool needSoftStop = false;
+
+    public void Start()
+    {
+        thread = new Thread(Work);
+        thread.Start();
+    }
+
+    public void AddCommand(ICommand command)
+    {
+        queue.Add(command);
+    }
+
+    public void ServerJoin()
+    {
+        thread?.Join();
+    }
+
+    private void Work()
+    {
+        while (!needHardStop)
+        {
+            ICommand command;
+            try
+            {
+                command = queue.Take();
+            }
+            catch
+            {
+                break;
+            }
+            
+            command.Execute();
+
+            if (needSoftStop && queue.Count == 0)
+            {
+                needHardStop = true;
+            }
+        }
+    }
+
+    public void HardStop()
+    {
+        if (Thread.CurrentThread != thread)
+            throw new InvalidOperationException("hardstop only from current thread");
+        
+        needHardStop = true;
+        queue.CompleteAdding();
+    }
+
+    public void SoftStop()
+    {
+        if (Thread.CurrentThread != thread)
+            throw new InvalidOperationException("softstop only from current thread");
+        
+        needSoftStop = true;
+        queue.CompleteAdding();
+    }
+}
+
+public class HardStop : ICommand
+{
+    private ServerThread server;
+    public HardStop(ServerThread server) { this.server = server; }
+    public void Execute() { server.HardStop(); }
+}
+
+public class SoftStop : ICommand
+{
+    private ServerThread  server;
+    public SoftStop(ServerThread server) { this.server = server; }
+    public void Execute() { server.SoftStop(); }
+}
